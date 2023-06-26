186d1785687f71300702dabe8eeddbb010f23c58
MrFlava <thatelitemaili33t@gmail.com>
2023-06-26 15:28:35 +0300

    Initial commit: settings

--- social_network/settings.py
+++ social_network/settings.py
@@ -28,12 +28,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django modules
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party modules
+    'rest_framework',
+
+    # apps
 ]
 
 MIDDLEWARE = [
@@ -118,6 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 
 try:
